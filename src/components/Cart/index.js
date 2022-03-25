95078de35d322e2715ebfc272b41edc6c3b0d8bf
Michael Morales <mt.morales@hotmail.fr>
2022-03-25 13:17:02 +0100

    empty cart variant done

--- src/components/Cart/index.js
+++ src/components/Cart/index.js
@@ -2,6 +2,7 @@ import { useContext } from "react";
 import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
 
 import CartItem from "./CartItem";
 import Title from "../reusable/Title";
@@ -15,19 +16,28 @@ const Cart = () => {
     <Box>
       <Title content={"mon panier"} />
 
-      <Container maxWidth="sm">
-        <Grid
-          container
-          direction="column"
-          alignItems="stretch"
-          spacing={{ xs: 2, sm: 4 }}
-          mb={2}
-        >
-          {cart.map((item) => (
-            <CartItem key={item.product.id} {...item} />
-          ))}
-        </Grid>
-      </Container>
+      {cart.length === 0 && (
+        <Box mt={20}>
+          <Typography component="h2" variant="h5" align="center">
+            Aucun produit dans votre panier.
+          </Typography>
+        </Box>
+      )}
+      {cart.length > 0 && (
+        <Container maxWidth="sm">
+          <Grid
+            container
+            direction="column"
+            alignItems="stretch"
+            spacing={{ xs: 2, sm: 4 }}
+            mb={2}
+          >
+            {cart.map((item) => (
+              <CartItem key={item.product.id} {...item} />
+            ))}
+          </Grid>
+        </Container>
+      )}
     </Box>
   );
 };
